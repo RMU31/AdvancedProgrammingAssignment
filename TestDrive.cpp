8f702b3056ed4d93332bd88325c2a1fa7ae25a56
RMU31 <rachana.mudrakolla@sky.uk>
2022-01-29 23:05:21 +0000

    time test drive

--- TestDrive.cpp
+++ TestDrive.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <time.h>
+
+using namespace std;
 
 int main() {
-    std:: cout << "Hello to the World, this is a test drive. Please Work!!" << std::endl;
+    // string name;
+    // int age;
+
+
+    // cout << "Hello to the World, Please enter your name!";
+    // cin >> name;
+
+
+    // cout << "hello, " << name << endl;
+
+    time_t rawtime;
+    struct tm* timeinfo;
+    char buffer[32];
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    // localtime(&timeinfo, &natime);
+    // asctime(buffer, 32, &timeinfo);
+    strftime(buffer, 32, "Time is %I:%M%p.", timeinfo);
+
+    cout << "The current time is: " << buffer << endl;
+    cout << timeinfo->tm_hour << ":" << timeinfo->tm_min << ":" << timeinfo->tm_sec << endl;
+
 }
